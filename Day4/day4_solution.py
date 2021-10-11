34730110503333b8128b4a95f9f221fb69a2a48b
Özge Tufan <ozgetufann97@gmail.com>
2021-10-11 19:46:03 +0200

    Part2 solution

--- Day4/day4_solution.py
+++ Day4/day4_solution.py
@@ -18,7 +18,7 @@ else:
     big_list.append(small_list)
     small_list = []
 
-print(big_list)
+# print(big_list)
 for ele in big_list:
     if len(ele) == 1:
         for x in ele:
@@ -52,8 +52,8 @@ for x in splitted_list:
 #         if all(["cid" not in field for field in x]):
 #             valid_passports += 1
 
-print(splitted_list)
-print(len(valid_test))
+# print(splitted_list)
+# print(len(valid_test))
 
 
 #Part 2
@@ -70,56 +70,48 @@ for each_dict in dict_list:
         if x == 'eyr' or x == 'byr' or x == 'iyr': #pid cannot be add because there is a 161cm value somewhere!!.
             each_dict[x] = int(each_dict.get(x)) #set the values to integer
 
-print(len(dict_list))
-
-# Check four digits and eyr
+invalid_list = []
+# Check eyr
 for each_dict in dict_list:
-    if len(str(each_dict['eyr'])) != 4 or len(str(each_dict['byr'])) != 4 or len(str(each_dict['iyr'])) != 4:
-        dict_list.remove(each_dict)
     if each_dict['eyr'] < 2020 or each_dict['eyr'] > 2030:
-        dict_list.remove(each_dict)
-
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 # Check byr
 for each_dict in dict_list:
     if each_dict['byr'] < 1920 or each_dict['byr'] > 2002:
-        dict_list.remove(each_dict)
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
 # Check iyr
 for each_dict in dict_list:
     if each_dict['iyr'] < 2010 or each_dict['iyr'] > 2020:
-        dict_list.remove(each_dict)
-
-print(dict_list)
-# print(len(dict_list))
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
 # Check hgt
 for each_dict in dict_list:
     if len(each_dict['hgt']) == 5 or len(each_dict['hgt']) == 4:
         continue
     else:
-        dict_list.remove(each_dict)
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
-# Check hgt -- cm
 for each_dict in dict_list:
-    if len(each_dict['hgt']) == 5:
-        if each_dict['hgt'][3] == 'c' and each_dict['hgt'][4] == 'm':
-            if 149 < int(each_dict['hgt'][:3]) < 194:
-                continue
-            else:
-                dict_list.remove(each_dict)
+    if each_dict['hgt'][-2:] == 'cm':
+        if 149 < int(each_dict['hgt'][:-2]) < 194:
+            continue
         else:
-            dict_list.remove(each_dict)
-
-# Check hgt -- inc
-for each_dict in dict_list:
-    if len(each_dict['hgt']) == 3:
-        if each_dict['hgt'][2] == 'i' and each_dict['hgt'][3] == 'n':
-            if 58 < int(each_dict['hgt'][:2]) < 77:
-                continue
-            else:
-                dict_list.remove(each_dict)
+            if each_dict not in invalid_list:
+                invalid_list.append(each_dict)
+    elif each_dict['hgt'][-2:] == 'in':
+        if 58 < int(each_dict['hgt'][:-2]) < 77:
+            continue
         else:
-            dict_list.remove(each_dict)
+            if each_dict not in invalid_list:
+                invalid_list.append(each_dict)
+    else:
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
 
 # Check hcl
@@ -127,20 +119,42 @@ for each_dict in dict_list:
     if each_dict['hcl'][0] == '#' and len(each_dict['hcl']) == 7:
         continue
     else:
-        dict_list.remove(each_dict)
-
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
 hcl_values = ['a', 'b', 'c', 'd', 'e', 'f', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 for each_dict in dict_list:
-    n = 0
     for x in each_dict['hcl'][1:]:
         if x not in hcl_values:
-            n = n + 1
-    if n != 0:
-        dict_list.remove(each_dict)
+            if each_dict not in invalid_list:
+                invalid_list.append(each_dict)
+            break
 
+# Check ecl
+ecl_values = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+for each_dict in dict_list:
+    if len(each_dict['ecl']) != 3:
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
+for each_dict in dict_list:
+    if each_dict['ecl'] not in ecl_values:
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
 
-print(len(dict_list))
+# Check pid
+for each_dict in dict_list:
+    if len(each_dict['pid']) != 9:
+
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
+for each_dict in dict_list:
+    try:
+        int(each_dict['pid'])
+    except ValueError:
+        if each_dict not in invalid_list:
+            invalid_list.append(each_dict)
+
+print(len(dict_list) - len(invalid_list))
 
 
 
