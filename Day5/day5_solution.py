f85e2fc193029369bae128e287dd453482973b0b
Özge Tufan <ozgetufann97@gmail.com>
2022-01-18 17:21:06 +0100

    Part2 solution

--- Day5/day5_solution.py
+++ Day5/day5_solution.py
@@ -2,7 +2,7 @@ with open('input_day5.txt') as f:
     lines = f.read().splitlines()
 
 print(lines)
-
+rows = []
 seat_ids = []
 for line in lines:
     a = 0
@@ -14,6 +14,7 @@ for line in lines:
             b = a + (b-a)//2
         elif x == "B":
             a = a + (b-a)//2 + 1
+    rows.append(b)
     for y in line[7:10]:
         if y == "L":
             d = c + (d-c)//2
@@ -22,7 +23,19 @@ for line in lines:
     seat_id = (a * 8) + c
     seat_ids.append(seat_id)
 
-print(max(seat_ids))
+##Part2
+possible = []
+
+for x in range(4, 115):
+    for y in range(0, 8):
+        seat = (x * 8) + y
+        if seat in seat_ids:
+            continue
+        else:
+            possible.append(seat)
+
+print(possible)
+
 
 
 
